cee85b19910447e49cc56dbe25f0b6dbfaefd7c5
thanaporn.ch <thanaporn_chan@cmu.ac.th>
2025-01-21 23:03:58 +0700

    #fixed: socket

--- api/hub.go
+++ api/hub.go
@@ -89,7 +89,7 @@ func (c *Client) readPump() {
 		c.hub.unregister <- c
 		c.conn.Close()
 	}()
-	c.conn.SetReadLimit(maxMessageSize)
+	// c.conn.SetReadLimit(maxMessageSize)
 	// c.conn.SetReadDeadline(time.Now().Add(pongWait))
 	c.conn.SetPongHandler(func(string) error {
 		// c.conn.SetReadDeadline(time.Now().Add(pongWait))
